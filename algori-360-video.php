af6dfedfd45a20ad6c2d0f6c878ee42c988d4855
KEVIN BAZIRA <kvnbzr@gmail.com>
2018-11-12 12:03:28 +0300

    updated version to 1.0.1

--- algori-360-video.php
+++ algori-360-video.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: 360 Video
+ * Plugin Name: Algori 360 Video
  * Plugin URI: https://github.com/kevinbazira/algori-360-video/
  * Description: <strong>360 Video</strong> is a Gutenberg Block Plugin that enables you add interactive 360° videos to your website. Adding immersive panorama videos, spherical videos, equirectangular videos and VR (Virtual Reality) videography <strong>will boost user engagement and increase revenue for your site</strong>.
  * Author: Kevin Bazira
  * Author URI: http://kevinbazira.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
